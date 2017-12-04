2726abbc14624feb037ba36af16566c276b485fb
ujjwal jindal <ujjwaljindal12@gmail.com>
2017-12-05 01:32:37 +0530

    new file

---
+++ RepeatAString.js
@@ -0,0 +1,14 @@
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  
+  
+  
+  if(num>0){
+  return str.repeat(num);
+}
+  else {
+    return "";
+  }
+}
+repeatStringNumTimes("abc", 3);
